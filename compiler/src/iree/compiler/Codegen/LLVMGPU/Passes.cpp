fdb6e4ec9f95a70ea5de1c59d844281eba18e0e9
Lukas Sommer <lukas.sommer@amd.com>
2026-03-27 16:15:43 +0000

    Lower and distribute async_dma to amdgpu.gather_to_lds
    Signed-off-by: Lukas Sommer <lukas.sommer@amd.com>

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
@@ -839,6 +839,7 @@ void addGPUVectorDistributePassPipeline(OpPassManager &funcPassManager,
 
   // Vector SIMD -> Vector SIMT
   funcPassManager.addPass(createLLVMGPUVectorDistributePass());
+  funcPassManager.addPass(createGPULowerAsyncDMAPass());
   funcPassManager.addPass(IREE::LinalgExt::createDecomposeMapStorePass());
   funcPassManager.addPass(createHoistInnerTiledAccReshapesPass());
   funcPassManager.addPass(IREE::GPU::createUnrollToIntrinsicsPass());
